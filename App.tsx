232eea535db13e8eb11c1a676e7467fba8d4ab92
caduxl007 <eduardo2020dev@gmail.com>
2021-04-21 21:23:31 -0300

    create screen welcome and install fonsts and icons

--- App.tsx
+++ App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import AppLoading from 'expo-app-loading';
 import { Welcome } from './src/pages/Welcome';
 
+import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost';
+
 export default function App() {
+  const [ fonstLoaded ] = useFonts({
+    Jost_400Regular, 
+    Jost_600SemiBold
+  });
+
+  if(!fonstLoaded) {
+    return <AppLoading />
+  }
+
   return(
-   <Welcome />
+    <Welcome />
   )
 }
