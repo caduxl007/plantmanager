3ec52f0c3b0cf158bea70ecf28b7af4fe7f789be
caduxl007 <eduardo2020dev@gmail.com>
2021-04-25 14:33:41 -0300

    create component the EnviromentButton

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -2,13 +2,26 @@ import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 
 import { Header } from '../components/Header';
+import { EnviromentButton } from '../components/EnviromentButton';
 
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function PlantSelect() {
   return (
     <View style={styles.container}>
-     <Header />
+      <View style={styles.header}>
+        <Header />
+
+        <Text style={styles.title}>
+          Em qual ambiente
+        </Text>
+        <Text style={styles.subtitle}>
+          você quer colocar sua planta?
+        </Text>
+      </View>
+
+      <EnviromentButton title="Cozinha" active />
     </View>
   )
 }
@@ -17,5 +30,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.background,
+  },
+  header: {
+    paddingHorizontal: 30,
+  },
+  title: {
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 20,
+    marginTop: 15
+  },
+  subtitle: {
+    fontFamily: fonts.text,
+    fontSize: 17,
+    lineHeight: 20,
+    color: colors.heading,
   }
 })
